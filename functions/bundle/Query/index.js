1112f32a15d86bc1d3b97ded333c0398df3d3314
Vibhor Mungee <vibhor.mungee@6degreesit.com>
2020-03-31 15:35:50 +0530

    bundle updated

--- functions/bundle/Query/index.js
+++ functions/bundle/Query/index.js
@@ -23,7 +23,19 @@ const Query = {
       id,
       ...reports[0][0]
     }
-  }
+  },
+  fatalityRateByAge: async () => {
+    const fatalityRate = await PluginManager.getFatalityRateByAge();
+    const id = uuid();
+    return { id, fatalityRate: fatalityRate[0] }
+  },
+  coronavirusHasSpread: async () => {
+    const spread = await PluginManager.getCountriesWhereCoronavirusHasSpread();
+    const id = uuid();
+    return { id, hasSpread: spread[0] }
+  },
 }
 
 module.exports = { Query }
+// const fatalityRateByAge = await PluginManager.getFatalityRateByAge()
+// const countriesWhereCoronavirusHasSpread = await PluginManager.getCountriesWhereCoronavirusHasSpread()
