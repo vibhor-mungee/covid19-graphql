10694e8f093f7fa710e6f7b8207415e4150b49e8
nitin <nitin.dhote@6degreesit.com>
2020-04-04 10:28:10 +0530

    after run bundle

--- functions/bundle/Query/index.js
+++ functions/bundle/Query/index.js
@@ -28,14 +28,26 @@ const getDateWiseReport = async () => {
 };
 
 const getCountryWiseNews = async (country) => {
-  return await new Promise(resolve =>
-    request({
+  let headers = "";
+  if (country) {
+    headers = {
       url: `https://newsapi.org/v2/top-headlines?apiKey=63749fa5401b47c38fc99a7efb35e65a&category=health&country=${country}&q=corona`,
       method: 'get',
       headers: {
         "Content-Type": "application/json"
       },
-    }, (e, r, body) => {
+    }
+  } else {
+    headers = {
+      url: `https://newsapi.org/v2/top-headlines?apiKey=63749fa5401b47c38fc99a7efb35e65a&category=health&q=corona`,
+      method: 'get',
+      headers: {
+        "Content-Type": "application/json"
+      },
+    }
+  }
+  return await new Promise(resolve =>
+    request(headers, (e, r, body) => {
       if (!body) {
         return resolve([]);
       }
@@ -135,16 +147,26 @@ const Query = {
     return { id, data: data };
   },
   getCountryNews: async (parent, { country }, context) => {
-    const cache = myCache.get(country);
+    const cache = await myCache.get(country);
     let id = uuid();
     if (!cache) {
       const ttl = 60 * 60 * 6;
-      let countryCode = countries.getAlpha2Code(country, 'en');
+      let countryCode = await countries.getAlpha2Code(country, 'en');
       let result = await getCountryWiseNews(countryCode);
-      myCache.set(country, result.articles, ttl);
+      if (result.articles && result.articles.length > 0) {
+        myCache.set(country, result.articles, ttl);
+      } else {
+        let globalCache = await myCache.get("global");
+        if (!globalCache) {
+          result = await getCountryWiseNews(null);
+          myCache.set("global", result.articles, ttl);
+        } else {
+          return { id: `${country} cache`, news: globalCache }
+        }
+      }
       return { id, news: result.articles };
     }
-    return { id: "cache", news: cache }
+    return { id: `${country} cache`, news: cache }
   }
 }
 
